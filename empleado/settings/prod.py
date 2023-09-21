d238caaa2968b1811607f6516d5fffc09531582e
jasosalgado <jaso_98@hotmail.com>
2023-09-21 12:00:09 -0600

    ADD: prod.py

---
+++ empleado/settings/prod.py
@@ -0,0 +1,24 @@
+from decouple import config
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT'),        
+    }
+}
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = ['static/']
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
